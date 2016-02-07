6e5e186aea974374e74a4601f68f9b82c82c6f51
Brian Redd <brianredd@gmail.com>
2016-02-07 11:02:37 -0700

    Fixes spacing issues with JavaScript InsertionSort

--- JavaScript/InsertionSort/src/InsertionSort.js
+++ JavaScript/InsertionSort/src/InsertionSort.js
@@ -3,38 +3,38 @@
 
   InsertionSort.byAuthor = function(_books) {
     for(var i = 1; i < _books.length; i++) {
-		  var j = i - 1;
+      var j = i - 1;
       var insert = _books[i];
-		  while(j >= 0 && _books[j].compareAuthor(insert) > 0) {
-			  _books[j+1] = _books[j];
-			  j--;
-		  }
-		  _books[j+1] = insert;
-	  }
+      while(j >= 0 && _books[j].compareAuthor(insert) > 0) {
+        _books[j+1] = _books[j];
+        j--;
+      }
+      _books[j+1] = insert;
+    }
   }
 
   InsertionSort.byNumberPages = function(_books) {
     for(var i = 1; i < _books.length; i++) {
-		  var j = i - 1;
+      var j = i - 1;
       var insert = _books[i];
-		  while(j >= 0 && _books[j].compareNumberPages(insert) > 0) {
-			  _books[j+1] = _books[j];
-			  j--;
-		  }
-		  _books[j+1] = insert;
-	  }
+      while(j >= 0 && _books[j].compareNumberPages(insert) > 0) {
+        _books[j+1] = _books[j];
+        j--;
+      }
+      _books[j+1] = insert;
+    }
   }
 
   InsertionSort.byTitle = function(_books) {
     for(var i = 1; i < _books.length; i++) {
-		  var j = i - 1;
+      var j = i - 1;
       var insert = _books[i];
-		  while(j >= 0 && _books[j].compareTitle(insert) > 0) {
-			  _books[j+1] = _books[j];
-			  j--;
-		  }
-		  _books[j+1] = insert;
-	  }
+      while(j >= 0 && _books[j].compareTitle(insert) > 0) {
+        _books[j+1] = _books[j];
+        j--;
+      }
+      _books[j+1] = insert;
+    }
   }
 
   module.exports = InsertionSort;
